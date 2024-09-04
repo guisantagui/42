a0b2b8836541cd8c9649e0fd8f8b5914ab09122c
guisantagui <gsantamaria@fc.ul.pt>
2024-09-04 18:05:29 +0200

    Add C01

---
+++ piscine/C01_wMains/ex00/ft_ft.c
@@ -0,0 +1,48 @@
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if(nb <= 2147483647 && nb >= -2147483648)
+	{
+		if (nb == -2147483648)
+		{
+			ft_putchar('-');
+			ft_putchar('2');
+			ft_putnbr(147483648);
+		}
+		else if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+			ft_putnbr(nb);
+		}
+		else if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else
+			ft_putchar(nb + '0');
+	}
+	
+}
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+int main()
+{
+	int	n;
+
+	ft_ft(&n);
+	ft_putnbr(n);
+	return(0);
+}
---
+++ piscine/C01_wMains/ex01/ft_ultimate_ft.c
@@ -0,0 +1,57 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putnbr(int nb)
+{
+	if(nb <= 2147483647 && nb >= -2147483648)
+	{
+		if (nb == -2147483648)
+		{
+			ft_putchar('-');
+			ft_putchar('2');
+			ft_putnbr(147483648);
+		}
+		else if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+			ft_putnbr(nb);
+		}
+		else if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else
+			ft_putchar(nb + '0');
+	}
+	
+}
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+int main()
+{
+	int	n;
+
+	int	*ptr1 = &n;
+	int	**ptr2 = &ptr1;
+	int	***ptr3 = &ptr2;
+	int	****ptr4 = &ptr3;
+	int	*****ptr5 = &ptr4;
+	int	******ptr6 = &ptr5;
+	int	*******ptr7 = &ptr6;
+	int	********ptr8 = &ptr7;
+	int	*********ptr9 = &ptr8;
+	ft_ultimate_ft(ptr9);
+	ft_putnbr(n);
+	return(0);
+}
+
