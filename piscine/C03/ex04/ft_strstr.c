066cae8aab294231cea73b1821655cf78e4b8b83
Guillem Santamaria Aguilar <gsantama@c1r1s4.42luxembourg.lu>
2024-09-10 18:18:58 +0200

    Pass norminette

--- piscine/C03/ex04/ft_strstr.c
+++ piscine/C03/ex04/ft_strstr.c
@@ -1,11 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsantama <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/10 17:54:03 by gsantama          #+#    #+#             */
+/*   Updated: 2024/09/10 18:18:08 by gsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 //#include <stdio.h>
 //#include <string.h>
+
+char	*substr_chk(char *str, char *to_find, int pos)
+{
+	int	f_match;
+	int	i;
+
+	f_match = pos;
+	i = 0;
+	while (str[pos] == to_find[i] && to_find[i] != '\0' && str[pos] != '\0')
+                        {
+                                i++;
+                                pos++;
+                        }
+                        if (to_find[i] == '\0')
+                                return (str + f_match);
+                        else
+                                return (NULL);
+}
 
 char	*ft_strstr(char *str, char *to_find)
 {
 	int	i;
 	int	j;
-	int	f_match;
+	char	*p_out;
 
 	i = 0;
 	j = 0;
@@ -15,34 +45,24 @@ char	*ft_strstr(char *str, char *to_find)
 	{
 		if (str[j] == to_find[i])
 		{
-			f_match = j;
-			while (str[j] == to_find[i] && to_find[i] != '\0' && str[j] != '\0')
-			{
-				i++;
-				j++;
-			}
-			if(to_find[i] == '\0')
-				return (str + f_match);
-			else
-				i = 0;	
+			p_out = substr_chk(str, to_find, j);
+			if (p_out != NULL)
+				return (p_out);
 		}
 		j++;
 	}
-	return (NULL);	
+	return (NULL);
 }
 
 /*int main() {
     char str[] = "Hello, world!";
     char sub[] = "world";
 
-    // Find the first occurrence of "world" in "Hello, world!"
     char *result = ft_strstr(str, sub);
-
     if (result != NULL) {
-        printf("Substring found: %s\n", result);  // Prints the rest of the string starting from "world"
+        printf("Substring found: %s\n", result);
     } else {
         printf("Substring not found.\n");
     }
-
     return 0;
 }*/
