f9f6241ca6921446f62b5493e2a6cdb74cbb4d1e
Guillem Santamaria Aguilar <guillemsantamariaaguilar@MacBook-Pro-de-Guillem.local>
2024-09-20 02:23:31 +0200

    Fix C05

--- piscine/C05_wMains/ex05/ft_sqrt.c
+++ piscine/C05_wMains/ex05/ft_sqrt.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+/*
 int	ft_sqrt(int nb)
 {
 	int	i;
@@ -18,18 +18,19 @@ int	ft_sqrt(int nb)
 		return (0);
 	}
 }
-/*
+*/
 int	sqrt_help(int nb, int g)
 {
 	if (nb < 0 || nb > 2147395600)
 		return (0);
 	else if (nb == 0 || nb == 1)
 		return (nb);
-	if (g * g == nb)
+	else if (g * g == nb)
 		return (g);
-	if (g * g < nb && (g + 1) * (g + 1) > nb)
+	else if (g * g < nb && (g + 1) * (g + 1) > nb)
 		return (0);
-	return(sqrt_help(nb, (g + nb / g) / 2));
+	else
+		return(sqrt_help(nb, (g + nb / g) / 2));
 }
 
 int	ft_sqrt(int nb)
@@ -39,7 +40,7 @@ int	ft_sqrt(int nb)
 	g = nb / 2;
 	return (sqrt_help(nb, g));
 }
-*/
+
 int	main()
 {
 	int	num = 26;
@@ -48,10 +49,20 @@ int	main()
 	int     num3 = 100;
 	int	num4 = -4;
 	int	num5 = 2147483647;
+	int	num6 = 2147395599;
+	int     num7 = 3;
+	int	num8 = 9;
+	int     num9 = 0;
+	int     num10 = 1000000;
 	printf ("sqrt(%d) = %d\n", num, ft_sqrt(num));
 	printf ("sqrt(%d) = %d\n", num1, ft_sqrt(num1));
 	printf ("sqrt(%d) = %d\n", num2, ft_sqrt(num2));
 	printf ("sqrt(%d) = %d\n", num3, ft_sqrt(num3));
 	printf ("sqrt(%d) = %d\n", num4, ft_sqrt(num4));
 	printf ("sqrt(%d) = %d\n", num5, ft_sqrt(num5));
+	printf ("sqrt(%d) = %d\n", num6, ft_sqrt(num6));
+	printf ("sqrt(%d) = %d\n", num7, ft_sqrt(num7));
+	printf ("sqrt(%d) = %d\n", num8, ft_sqrt(num8));
+	printf ("sqrt(%d) = %d\n", num9, ft_sqrt(num9));
+	printf ("sqrt(%d) = %d\n", num10, ft_sqrt(num10));
 }
