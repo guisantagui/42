e9c02a8b756a20135bfa3ed522142ec5b050dd94
Guillem Santamaria Aguilar <gsantama@c1r1s2.42luxembourg.lu>
2024-09-23 23:01:30 +0200

    Corrections on split

--- piscine/C09/ex02/ft_split.c
+++ piscine/C09/ex02/ft_split.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-
+#include <stdio.h>
 
 int	is_sep(char c, char *charset)
 {
@@ -37,15 +37,33 @@ int	word_count(char *str, char *charset)
 	return (count);
 }
 
-char	*alloc_word(int len)
+int	strlen_til_sep(char *str, char *charset)
 {
-	char	*word;
+	int	i;
 
-	word = (char *)malloc((len + 1) * sizeof(char));
-	return (word);
+	i = 0;
+	while (str[i] != '\0' && is_sep(str[i], charset) == 0)
+		i++;
+	return (0);
 }
 
+int	alloc_word(char *word, char *str, int pos, char *charset)
+{
+	int	len;
+	int	c_idx;
 
+	len = strlen_til_sep(str + pos, charset);
+	c_idx = 0;
+	word = (char *)malloc((len + 1) * sizeof(char));
+	while (is_sep(str[pos], charset) == 0 && str[pos] != '\0')
+	{
+		word[c_idx] = str[pos];
+		pos++;
+		c_idx++;
+	}
+	word[c_idx] = '\0';
+	return (pos);
+}
 
 char	**ft_split(char *str, char *charset)
 {
@@ -53,23 +71,16 @@ char	**ft_split(char *str, char *charset)
 	char	**split;
 	int	i;
 	int	w_idx;
-	int	c_idx;
 
 	word_num = word_count(str, charset);
 	split = (char *)malloc((word_num + 1) * sizeof(char));
 	i = 0;
 	w_idx = 0;
-	c_idx = 0;
 	while (str[i])
 	{
 		while (is_sep(str[i], charset) == 1)
 			i++;
-		while (is_sep(str[i], charset) == 0)
-		{
-			split[w_idx][c_idx] = str[i];
-			i++;
-			c_idx++;
-		}
+		i = alloc_word(split[w_idx], str, i, charset);
 		if (is_sep(str[i], charset) == 1)
 		{
 			split[w_idx][c_idx] = '\0';
@@ -77,4 +88,22 @@ char	**ft_split(char *str, char *charset)
 			c_idx = 0;
 		}
 	}
+	split[w_idx] = 0;
+	return (split);
 }
+
+int	main()
+{
+	char to_split = "Hola, aqui.estamos:quepasa";
+	char sep = ",.:";
+	char **split = ft_split(to_split, sep);
+	int	w;
+
+	w = 0;
+	while (split[w] != 0)
+	{
+		printf("%s\n", split[w]);
+		w++;
+	}
+}
+
