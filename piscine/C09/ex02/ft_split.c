ad76ed39ed1f9680b413cfb9c20b3441603cc279
guisantagui <gsantamaria@fc.ul.pt>
2024-09-24 02:21:35 +0200

    Complete C09

--- piscine/C09/ex02/ft_split.c
+++ piscine/C09/ex02/ft_split.c
@@ -10,6 +10,7 @@ int	is_sep(char c, char *charset)
 	{
 		if (charset[i] == c)
 			return (1);
+		i++;
 	}
 	return (0);
 }
@@ -44,24 +45,24 @@ int	strlen_til_sep(char *str, char *charset)
 	i = 0;
 	while (str[i] != '\0' && is_sep(str[i], charset) == 0)
 		i++;
-	return (0);
+	return (i);
 }
 
-int	alloc_word(char *word, char *str, int pos, char *charset)
+int	alloc_word(char **word, char *str, int pos, char *charset)
 {
 	int	len;
 	int	c_idx;
 
 	len = strlen_til_sep(str + pos, charset);
 	c_idx = 0;
-	word = (char *)malloc((len + 1) * sizeof(char));
+	*word = (char *)malloc((len + 1) * sizeof(char));
 	while (is_sep(str[pos], charset) == 0 && str[pos] != '\0')
 	{
-		word[c_idx] = str[pos];
+		(*word)[c_idx] = str[pos];
 		pos++;
 		c_idx++;
 	}
-	word[c_idx] = '\0';
+	(*word)[c_idx] = '\0';
 	return (pos);
 }
 
@@ -73,37 +74,37 @@ char	**ft_split(char *str, char *charset)
 	int	w_idx;
 
 	word_num = word_count(str, charset);
-	split = (char *)malloc((word_num + 1) * sizeof(char));
+	split = (char **)malloc((word_num + 1) * sizeof(char *));
 	i = 0;
 	w_idx = 0;
-	while (str[i])
+	while (str[i] && (w_idx < word_num))
 	{
 		while (is_sep(str[i], charset) == 1)
 			i++;
-		i = alloc_word(split[w_idx], str, i, charset);
-		if (is_sep(str[i], charset) == 1)
-		{
-			split[w_idx][c_idx] = '\0';
+		i = alloc_word(&split[w_idx], str, i, charset);
+		if (str[i] != '\0')
 			w_idx++;
-			c_idx = 0;
-		}
 	}
-	split[w_idx] = 0;
+	split[word_num] = 0;
 	return (split);
 }
 
-int	main()
+int	main(int argc, char **argv)
 {
-	char to_split = "Hola, aqui.estamos:quepasa";
-	char sep = ",.:";
-	char **split = ft_split(to_split, sep);
-	int	w;
-
-	w = 0;
-	while (split[w] != 0)
+	if (argc == 3)
 	{
-		printf("%s\n", split[w]);
-		w++;
-	}
-}
+		char *to_split = argv[1];
+		char *sep = argv[2];
+		char **split = ft_split(to_split, sep);
+		int	w;
 
+		w = 0;
+		while (split[w] != NULL)
+		{
+			printf("%s\n", split[w]);
+			w++;
+		}
+	}
+	else
+		printf("\n");
+}
