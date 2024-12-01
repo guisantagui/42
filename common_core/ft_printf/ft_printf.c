ca07efd5bca4df6e52bf1ce17c5720b8832aea6c
Guillem Santamaria Aguilar <guillemsantamariaaguilar@MacBook-Pro-de-Guillem.local>
2024-12-01 16:51:59 +0100

    Advance ft_printf.c

--- common_core/ft_printf/ft_printf.c
+++ common_core/ft_printf/ft_printf.c
@@ -10,11 +10,46 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
 #include <stdarg.h>
+#include <unistd.h>
+#include <stdio.h>
 
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+static	
+
+/*
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
-	va_start(args, count);
+	int	i;
+	int	p_chars;
+
+	va_start(args, format);
+	i = 0;
+	p_chars = 0;
+	while (format[i])
+	{
+		if (format[i] != '%')
+			ft_putchar(format[i]);
+		else
+		{
+			i++;
+			if (format[i] == 'd')
+			{
+				
+			}
+		}
+		
+	}
+}
+*/
+
+int	main()
+{
+	printf("%s, %s, %s y %s\n", "Jose", "Jesus", "Maria", "Gaspar");
 }
