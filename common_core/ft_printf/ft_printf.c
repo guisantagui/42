92a46551b69fac290bf9c872f3b37497e89c5e12
Guillem Santamaria Aguilar <gsantama@c1r3s5.42luxembourg.lu>
2024-12-01 19:01:02 +0100

    Fix ft_strncmp.c

--- common_core/ft_printf/ft_printf.c
+++ common_core/ft_printf/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: gsantama <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 19:48:22 by gsantama          #+#    #+#             */
-/*   Updated: 2024/11/30 20:02:59 by gsantama         ###   ########.fr       */
+/*   Updated: 2024/12/01 17:37:40 by gsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,48 @@
 #include <stdio.h>
 
 
-void	ft_putchar(char c)
+static void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-static	
+static void	ft_putstr(char *str)
+{
+	int	i;
 
-/*
+	if (!str)
+		return ;
+	i = 0;
+	while (str[i])
+		ft_putchar(str[i++]);
+}
+
+static void	ft_putnbr(int nb)
+{
+	if (nb <= 2147483647 && nb >= -2147483648)
+	{
+		if (nb == -2147483648)
+		{
+			ft_putchar('-');
+			ft_putchar('2');
+			ft_putnbr(147483648);
+		}
+		else if (nb < 0)
+		{
+			ft_putchar('-');
+			nb = -nb;
+			ft_putnbr(nb);
+		}
+		else if (nb > 9)
+		{
+			ft_putnbr(nb / 10);
+			ft_putnbr(nb % 10);
+		}
+		else
+			ft_putchar(nb + '0');
+	}
+}
+
 int	ft_printf(const char *format, ...)
 {
 	va_list	args;
@@ -39,17 +73,19 @@ int	ft_printf(const char *format, ...)
 		else
 		{
 			i++;
-			if (format[i] == 'd')
-			{
-				
-			}
+			if (format[i] == 's')
+				ft_putstr(va_arg(args, char *));
+			else if (format[i] == 'c')
+				ft_putchar((char)va_arg(args, int));
+
 		}
-		
+		i++;
 	}
 }
-*/
+
 
 int	main()
 {
-	printf("%s, %s, %s y %s\n", "Jose", "Jesus", "Maria", "Gaspar");
+	printf("%s, %c\n", "Jose", 'J');
+	ft_printf("%s, %c\n", "Jose", 'J');
 }
