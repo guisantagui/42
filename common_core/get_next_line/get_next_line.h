55cf21c7d9496a2f4c1a6edefba006cc334db51c
Guillem Santamaria Aguilar <gsantama@c3r1s3.42luxembourg.lu>
2025-01-13 18:22:56 +0100

    Fix norminette in .h

--- common_core/get_next_line/get_next_line.h
+++ common_core/get_next_line/get_next_line.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gsantama <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 18:21:15 by gsantama          #+#    #+#             */
+/*   Updated: 2025/01/13 18:21:59 by gsantama         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
@@ -8,10 +20,10 @@
 #  define BUFFER_SIZE 42
 # endif
 
-char     *get_next_line(int fd);
+char	*get_next_line(int fd);
 char	*ft_strdup(const char *s1);
-size_t  ft_strlen(const char *str);
-size_t  ft_strlcpy(char *dst, const char *src, size_t dstsize);
-char    *ft_strjoin_free(char const *s1, char const *s2, int free_both);
+size_t	ft_strlen(const char *str);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin_free(char const *s1, char const *s2, int free_both);
 
 #endif
